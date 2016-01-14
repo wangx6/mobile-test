38ab58ed8282f525cf081d4e08df837b3a94b802
wangx6 <wangx6@gmail.com>
2016-01-14 17:28:49 +0000

    swipe

--- js/main.js
+++ js/main.js
@@ -4,32 +4,44 @@
     var menu;
     var mask;
     var menuBtn;
+    var body;
 
     $$(document).ready(function() {
-    	initRef();
-	    menuBtn.tap(function() {
-	    	toggleMaks(true);
+        initRef();
+        menuBtn.tap(function() {
+            toggleMaks(true);
             menu.style('transform', 'translateX(0)').addClass('menu--show');
         });
+
+        body.swipeRight(function() {
+            toggleMaks(true);
+            menu.style('transform', 'translateX(0)').addClass('menu--show');
+        });
+
+        body.swipeLeft(function() {
+            menu.style('transform', 'translateX(-100%)');
+            toggleMaks(false);
+        });
+
     });
 
-    var toggleMaks = function( flag ) {
-    	if(mask) {
-    		mask.style('display', flag ? 'block' : 'none');
-    	}
+    var toggleMaks = function(flag) {
+        if (mask) {
+            mask.style('display', flag ? 'block' : 'none');
+        }
     };
 
     var initRef = function() {
-    	menu = $$('#side-menu');
-    	mask = $$('#page-mask');
-    	menuBtn = $$('#button-1');
-    	mask.tap(_onMaskTap);
+        menu = $$('#side-menu');
+        mask = $$('#page-mask');
+        menuBtn = $$('#button-1');
+        body = $$('body');
+        mask.tap(_onMaskTap);
     };
 
     var _onMaskTap = function() {
-    	menu.style('transform', 'translateX(-100%)');
-    	toggleMaks(false);
+        menu.style('transform', 'translateX(-100%)');
+        toggleMaks(false);
     };
-
 
 })();
