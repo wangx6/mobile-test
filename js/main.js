4dde5f0641343f93e5dcfe60566143f9cbdaf3bb
wangx6 <wangx6@gmail.com>
2016-01-14 16:21:28 +0000

    side menu done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
 (function() {
-	'use strict';
-	console.log($$);
-	$$(document).ready(function() {
-		$$('#button-1').tap(function() {
-			alert('###');
-		});		
-	});
+    'use strict';
+
+    var menu;
+    var mask;
+
+    $$(document).ready(function() {
+    	initRef();
+	    $$('#button-1').tap(function() {
+	    	toggleMaks(true);
+            menu.style('transform', 'translateX(0)').addClass('menu--show');
+        });
+    });
+
+    var toggleMaks = function( flag ) {
+    	if(mask) {
+    		mask.style('display', flag ? 'block' : 'none');
+    	}
+    };
+
+    var initRef = function() {
+    	menu = $$('#side-menu');
+    	mask = $$('#page-mask');
+    	mask.tap(_onMaskTap);
+    };
+
+    var _onMaskTap = function() {
+    	menu.style('transform', 'translateX(-100%)');
+    	toggleMaks(false);
+    };
+
+
 })();
